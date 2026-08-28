c8907a0d611486edd8f84104e8e1b33b3acba1c5
Aleksander <lolwolfyou@gmail.com>
2026-08-29 01:44:51 +0200

    feat: added getting player x,y,z and head pos

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,63 @@
 mod offsets;
 mod memory;
-use crate::memory::lib_memory::{get_module_base_address, read_bytes_from_memory};
+use crate::memory::lib_memory::{get_gmod_process_id, get_module_base_address, read_f32_bytes_from_memory, read_i32_bytes_from_memory};
 
 use std::process;
 use std::ffi::c_void;
+use std::ops::Add;
 use windows::Win32::System::Diagnostics::ToolHelp::{CreateToolhelp32Snapshot, Process32First, PROCESSENTRY32, TH32CS_SNAPPROCESS, Process32Next, TH32CS_SNAPMODULE, TH32CS_SNAPMODULE32, Module32FirstW, Module32NextW};
 use windows::Win32::System::Threading::{OpenProcess, PROCESS_QUERY_INFORMATION, PROCESS_VM_OPERATION, PROCESS_VM_READ, PROCESS_VM_WRITE};
 use crate::offsets::OFFSETS;
 
 
-// 71886314B4
 
 #[warn(unused_variables)]
 fn main() {
 
-    unsafe {
-        let handle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0).unwrap_or_else(|e| {
-            println!("CreateToolhelp32Snapshot Error: {}", e);
-            process::exit(1);
-        });
+    unsafe{
+        let gmod_pid = get_gmod_process_id().unwrap_or_else(|| panic!("Unable to get process id from gmod open the game first"));
 
-        let mut pe32: PROCESSENTRY32 = PROCESSENTRY32::default();
-        pe32.dwSize = size_of_val(&pe32) as u32;
+        let game_process =  OpenProcess(PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION | PROCESS_QUERY_INFORMATION, false, gmod_pid).unwrap();
 
-        println!("{:?}", size_of_val(&pe32) as u32);
+        println!("Starting game process {}", gmod_pid);
+        let client_dll = get_module_base_address("client.dll", gmod_pid).ok_or("couldnt find client.dll :(").unwrap();
+        let engine_dll = get_module_base_address("engine.dll", gmod_pid).ok_or("couldnt find engine.dll :(").unwrap();
 
-        Process32First(handle, &mut pe32).unwrap_or_else(|e| {
-            println!("Process32First Error: {}", e);
-            process::exit(1);
-        });
+        //
+        // let player_health_value = read_bytes_from_memory(game_process, player_health_address as *const c_void)
+        //     .ok_or("Failed to read final byte value")
+        //     .unwrap();
 
-        let mut game_pid: Option<u32> = None;
+        for i in 0..30 {
+            let entity_ptr = client_dll + OFFSETS.lock().unwrap().get("PLAYER_OFFSET").unwrap() + 0x0004 * i;
+            let read = read_i32_bytes_from_memory(game_process, entity_ptr as *const c_void);
 
-        loop {
-            let name = String::from_utf8(pe32.szExeFile.to_vec().iter().map(|x| *x as u8).collect()).unwrap_or_else(|e| {
-                println!("String::from_utf8 Error: {}", e);
-                process::exit(1);
-            });
-
-            if name.contains("gmod.exe"){
-                break game_pid = Some(pe32.th32ProcessID);
+            if read.is_none(){
+                continue;
             }
 
-            if let Err(_) = Process32Next(handle, &mut pe32) {
-                break;
+            let health_point_address = read.unwrap() as usize + OFFSETS.lock().unwrap().get("PLAYER_HEALTH_ADDRESS").unwrap();
+            let entity_health = read_i32_bytes_from_memory(game_process, health_point_address as *const c_void);
+
+            if entity_health.is_none() || entity_health.unwrap() <= 0{
+                continue;
             }
+
+            let red: Vec<u8> = vec![255,0,0];
+            // 026C x
+            // 0270 y
+            // 0274 z
+            let plr_x = read_f32_bytes_from_memory(game_process, (read.unwrap() as usize + 0x026C) as *const c_void).unwrap_or_else(|| panic!("couldnt find plr_x."));
+            let plr_y = read_f32_bytes_from_memory(game_process, (read.unwrap() + 0x0270) as *const c_void).unwrap_or_else(|| panic!("couldnt find plr_y."));
+            let plr_z = read_f32_bytes_from_memory(game_process, (read.unwrap() + 0x0274) as *const c_void).unwrap_or_else(|| panic!("couldnt find plr_z"));
+            let head_z = read_f32_bytes_from_memory(game_process, (read.unwrap() + 0x0274) as *const c_void).unwrap_or_else(|| panic!("couldnt find plr_head_z")) + 64.0;
+
+            println!("Starting plr game process {}, {}, {}", plr_x, plr_y, plr_z );
         }
 
-        let game_process = OpenProcess(PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION | PROCESS_QUERY_INFORMATION, false, game_pid.unwrap()).unwrap();
-
-        let client_dll = get_module_base_address("client.dll", game_pid.unwrap()).ok_or("Not found :(").unwrap();
-        let base_pointer_address = client_dll + OFFSETS.lock().unwrap().get("PLAYER_HEALTH").unwrap();
-
-        let first_pointer = read_bytes_from_memory(game_process, base_pointer_address as *const c_void)
-            .ok_or("Failed to read base pointer").unwrap();
-
-        let target_value_address = first_pointer + 0x0098;
-        let final_value = read_bytes_from_memory(game_process, target_value_address as *const c_void)
-            .ok_or("Failed to read final byte value").unwrap();
-
-        println!("health val {}", final_value as u8);
-
     }
+
+
+
 }
 
--- src/memory/lib_memory.rs
+++ src/memory/lib_memory.rs
@@ -1,7 +1,11 @@
 use std::ffi::c_void;
+use std::io::Error;
+use std::{f32, process};
 use windows::Win32::Foundation::{CloseHandle, HANDLE};
 use windows::Win32::System::Diagnostics::Debug::ReadProcessMemory;
-use windows::Win32::System::Diagnostics::ToolHelp::{CreateToolhelp32Snapshot, Module32NextW, MODULEENTRY32W, TH32CS_SNAPMODULE, Module32FirstW, TH32CS_SNAPMODULE32};
+use windows::Win32::System::Diagnostics::ToolHelp::{CreateToolhelp32Snapshot, Module32NextW, MODULEENTRY32W, TH32CS_SNAPMODULE, Module32FirstW, TH32CS_SNAPMODULE32, TH32CS_SNAPPROCESS, PROCESSENTRY32, Process32First, Process32Next};
+use windows::Win32::System::Threading::{OpenProcess, PROCESS_QUERY_INFORMATION, PROCESS_VM_OPERATION, PROCESS_VM_READ, PROCESS_VM_WRITE};
+use crate::offsets::OFFSETS;
 
 pub fn get_module_base_address(module_name: &str, pid: u32) -> Option<usize> {
     unsafe {
@@ -17,7 +21,7 @@ pub fn get_module_base_address(module_name: &str, pid: u32) -> Option<usize> {
                 let name = String::from_utf16_lossy(
                     &entry.szModule[..entry.szModule.iter().position(|&c| c == 0).unwrap_or(0)]
                 );
-                println!("ModuleBase Address: {}", name);
+
                 if name.eq_ignore_ascii_case(module_name) {
                     res = Some(entry.modBaseAddr as usize);
                     break;
@@ -35,7 +39,7 @@ pub fn get_module_base_address(module_name: &str, pid: u32) -> Option<usize> {
     }
 }
 
-pub fn read_bytes_from_memory(process: HANDLE, address: *const c_void) -> Option<i32> {
+pub fn read_i32_bytes_from_memory(process: HANDLE, address: *const c_void) -> Option<i32> {
     let mut buffer = [0u8; 4];
     let mut bytes_read= 0;
 
@@ -53,5 +57,58 @@ pub fn read_bytes_from_memory(process: HANDLE, address: *const c_void) -> Option
         Some(i32::from_le_bytes(buffer))
     } else {
         None
+    }
+}
+
+pub fn read_f32_bytes_from_memory(process: HANDLE, address: *const c_void) -> Option<f32> {
+    let mut buffer = [0u8; 4];
+    let mut bytes_read= 0;
+
+    let res = unsafe {
+        ReadProcessMemory(
+            process,
+            address,
+            buffer.as_mut_ptr() as *mut c_void,
+            buffer.len(),
+            Some(&mut bytes_read),
+        )
+    };
+
+    if res.is_ok() {
+        Some(f32::from_le_bytes(buffer))
+    } else {
+        None
+    }
+}
+
+pub fn get_gmod_process_id() -> Option<u32>{
+    unsafe {
+        let handle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0).unwrap_or_else(|e| {
+            println!("CreateToolhelp32Snapshot Error: {}", e);
+            process::exit(1);
+        });
+
+        let mut pe32: PROCESSENTRY32 = PROCESSENTRY32::default();
+        pe32.dwSize = size_of_val(&pe32) as u32;
+
+        Process32First(handle, &mut pe32).unwrap_or_else(|e| {
+            println!("Process32First Error: {}", e);
+            process::exit(1);
+        });
+
+        loop {
+            let name = String::from_utf8(pe32.szExeFile.to_vec().iter().map(|x| *x as u8).collect()).unwrap_or_else(|e| {
+                process::exit(1);
+            });
+
+            if name.contains("gmod.exe"){
+                break Some(pe32.th32ProcessID);
+            }
+
+            if let Err(_) = Process32Next(handle, &mut pe32) {
+                break None
+            }
+        }
+
     }
 }
